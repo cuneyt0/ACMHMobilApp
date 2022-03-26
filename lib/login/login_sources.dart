8040cc2023e4422eb9cb2ec1a2f23ae50a724f14
cuneyt.aykac@indbilisim.com.tr <BSmeZ2zTPoMBPup>
2022-03-26 12:58:58 +0300

    deneme

--- lib/login/login_sources.dart
+++
@@ -1,8 +0,0 @@
-class LoginResources {
-  final String title = "Seydişehir Ahmet Cengiz Mühendislik Fakültesi Sistemi";
-  final String buttonText = "Giriş Yap";
-  final String usernamelabeltext = "Okul Numarası";
-  final String usernamelhinttext = "Okul Numaranızı giriniz...";
-  final String passwordlabeltext = "TC kimlik numarası";
-  final String passwordlhinttext = "TC kimlik numaranızı giriniz...";
-}
