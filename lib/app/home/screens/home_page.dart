ecb7b643228549051054492effb50b2bd4481a19
cuneyt.aykac@indbilisim.com.tr <BSmeZ2zTPoMBPup>
2022-04-30 22:55:23 +0300

    HomeScreen de duyurular gösterildi.Tamamlandı.

--- lib/app/home/screens/home_page.dart
+++ lib/app/home/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:login_work/app/home/screens/announcements/screen/drawerMenu_Announcement_detail_screen.dart';
 import 'package:login_work/app/home/viewmodel/home_view_model.dart';
 
 import '../../../export_import.dart';
@@ -7,7 +8,8 @@ class HomePage extends StatefulWidget {
   final CacheManager? cacheManager;
   final bool? isClear;
   final HomeViewModel? viewModel;
-  const HomePage({Key? key, this.model, this.cacheManager, this.isClear,this.viewModel})
+  const HomePage(
+      {Key? key, this.model, this.cacheManager, this.isClear, this.viewModel})
       : super(key: key);
 
   @override
@@ -16,44 +18,108 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  void initState() {
-    // _viewModel?.getbyidrecently();
-    super.initState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: DrawerMenu(
+          context: context,
+          cacheManager: widget.cacheManager,
+          isClear: widget.isClear,
+          model: widget.model),
+      body: ListView(children: [
+        const Padding(
+          padding: EdgeInsets.only(top: 15.0),
+          child: Center(
+            child: Text(
+              mAnnouncement,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        HomeScreenAnnouncementWidget(widget: widget),
+      ]),
+    );
   }
+}
+
+class HomeScreenAnnouncementWidget extends StatelessWidget {
+  const HomeScreenAnnouncementWidget({
+    Key? key,
+    required this.widget,
+  }) : super(key: key);
+
+  final HomePage widget;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        drawer: DrawerMenu(
-            context: context,
-            cacheManager: widget.cacheManager,
-            isClear: widget.isClear,
-            model: widget.model),
-        body: Observer(
-            builder: (context) =>
-                widget.viewModel?.getbyidrecentlyresponse?.data == null
-                    ? Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : ListView.builder(
-                        itemCount:
-                           widget.viewModel?.getbyidrecentlyresponse?.data?.length,
-                        itemBuilder: (context, index) => ListTile(
-                          title: Text(widget.viewModel?.getbyidrecentlyresponse
-                                  ?.data?[index].title ??
-                              ""),
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.32,
+      child: Observer(
+        builder: (context) => widget.viewModel?.getbyidrecentlyresponse?.data ==
+                null
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : GridView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                padding:
+                    EdgeInsets.only(left: 5, right: 5, top: 20, bottom: 20),
+                itemCount:
+                    widget.viewModel?.getbyidrecentlyresponse?.data?.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1, mainAxisSpacing: 5, crossAxisSpacing: 5),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 1,
+                      height: 30,
+                      child: Card(
+                        child: Center(
+                          child: Text(
+                            widget.viewModel!.dateFormat(DateTime.parse(
+                                '${widget.viewModel?.getbyidrecentlyresponse?.data?[index].createdAt}')),
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      )));
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: ((context) => DrawerAnnouncementDetailScreen(
+                                model: widget.viewModel?.getbyidrecentlyresponse
+                                    ?.data?[index],
+                              )),
+                        ),
+                      ),
+                      child: Container(
+                        height: 100,
+                        width: MediaQuery.of(context).size.width * 1,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Padding(
+                          padding: widget.viewModel?.getbyidrecentlyresponse
+                                      ?.data?[index].title?.length.isOdd !=
+                                  false
+                              ? EdgeInsets.all(20)
+                              : EdgeInsets.all(0),
+                          child: Center(
+                            child: Text(
+                              widget.viewModel?.getbyidrecentlyresponse
+                                      ?.data?[index].title ??
+                                  "",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    );
   }
 }
-/*
-
-ListView.builder(
-          itemCount: widget.model?.userClaims?.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(widget.model?.userClaims?[index].name ?? ""),
-          ),
-        )
-
- */
--- lib/app/home/viewmodel/home_view_model.dart
+++ lib/app/home/viewmodel/home_view_model.dart
@@ -223,4 +223,86 @@ abstract class _HomeViewModelBase with Store {
       }
     }
   }
+
+  @action
+  String dateFormat(DateTime tm) {
+    @observable
+    DateTime today = new DateTime.now();
+    @observable
+    Duration oneDay = new Duration(days: 1);
+    @observable
+    Duration twoDay = new Duration(days: 2);
+    @observable
+    Duration oneWeek = new Duration(days: 7);
+    @observable
+    String? month;
+    switch (tm.month) {
+      case 1:
+        month = "Ocak";
+        break;
+      case 2:
+        month = "Şubat";
+        break;
+      case 3:
+        month = "Mart";
+        break;
+      case 4:
+        month = "Nisan";
+        break;
+      case 5:
+        month = "Mayıs";
+        break;
+      case 6:
+        month = "Haziran";
+        break;
+      case 7:
+        month = "Temmuz";
+        break;
+      case 8:
+        month = "Ağustos";
+        break;
+      case 9:
+        month = "Eylük";
+        break;
+      case 10:
+        month = "Ekim";
+        break;
+      case 11:
+        month = "Kasım";
+        break;
+      case 12:
+        month = "Aralık";
+        break;
+    }
+    @observable
+    Duration difference = today.difference(tm);
+
+    if (difference.compareTo(oneDay) < 1) {
+      return "Bugün";
+    } else if (difference.compareTo(twoDay) < 1) {
+      return "Dün";
+    } else if (difference.compareTo(oneWeek) < 1) {
+      switch (tm.weekday) {
+        case 1:
+          return "Pazartesi";
+        case 2:
+          return "Salı";
+        case 3:
+          return "Çarşamba";
+        case 4:
+          return "Perşembe";
+        case 5:
+          return "Cuma";
+        case 6:
+          return "Cumartesi";
+        case 7:
+          return "Pazar";
+      }
+    } else if (tm.year == today.year) {
+      return '${tm.day} $month';
+    } else {
+      return '${tm.day} $month ${tm.year}';
+    }
+    return "";
+  }
 }
